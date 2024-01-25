196752ad8591e932c1ca477402abbf941330b06a
kevinmkamel <152033397+kevinmkamel@users.noreply.github.com>
2024-01-25 14:35:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function security() {
     var password = prompt("برجاء ادخال الباسورد");
-    if (password == "123") {
+    if (password =="19281") {
       window.location.href = "https://forms.gle/WkmvWP2vYorgAGsu9";
     } else {
       alert("باسورد غلط برجاء اعادة المحاوله");
