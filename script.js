c4d51fb66ddf57e517e0fb98b1a7a5707f0bea47
kevinmkamel <152033397+kevinmkamel@users.noreply.github.com>
2024-01-29 16:09:57 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function security() {
     var password = prompt("برجاء ادخال الباسورد");
     if (password =="19281") {
-      window.location.href = "khodampage.html";
+      window.location.href = "https://kevinmkamel.github.io/admin/";
     } else {
       alert("باسورد غلط برجاء اعادة المحاوله");
     }
